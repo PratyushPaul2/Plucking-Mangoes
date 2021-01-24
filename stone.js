cff7afc3bd01af06cbce2cf84e82caf36e7aca90
PratyushPaul2 <74598606+PratyushPaul2@users.noreply.github.com>
2021-01-24 20:29:57 +0530

    Update stone.js

--- stone.js
+++ stone.js
@@ -2,7 +2,7 @@ class Stone extends BaseClass {
     constructor(x,y){
      
       super(x,y,20,20,);
-      this.image = loadImage("Images/stone.png");
+      this.image = loadImage("stone.png");
     }
   
     display() {
